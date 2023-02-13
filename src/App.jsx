f42bc6785aac7dcd476912cbcc28456da8cc9c17
Amanda Brundin <abrundin@kth.se>
2023-02-13 16:13:34 +0100

    bar chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
-import React, { useEffect } from 'react';
-import { csv } from 'd3';
 
-const csvUrl =
-  'https://gist.githubusercontent.com/Amandabru/00e96eaa56143e6499d1c651bac03aa8/raw/58ce042b4504d9b660bb93693e47b966cc2eb34f/GapminderData.csv?fbclid=IwAR21ILavgwgRxV4nzOa4tL-KAlwzUElOyUSlaw2lDXhdtYljSdIiXFH5GuM';
+
+import React, { useState, useCallback, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import {csv, request, scaleBand, scaleLinear, max} from 'd3';
+
+
+const csvUrl = 'https://gist.githubusercontent.com/Amandabru/00e96eaa56143e6499d1c651bac03aa8/raw/58ce042b4504d9b660bb93693e47b966cc2eb34f/GapminderData.csv';
+
+const width = 960;
+const height = 500;
+
 
 const App = () => {
+  const [data, setData] = useState(null);
+
   useEffect(() => {
-    csv(csvUrl).then((data) => {
-      console.log(data);
-    });
+    csv(csvUrl).then(setData);
   }, []);
+
+  if(!data){
+    return <pre>Loading...</pre>;
+  }
+
+
+  const xScale = scaleBand()
+  .domain(data.map(d => d.income))
+  .range([0, width]);
+
+  const yScale = scaleLinear()
+    .domain([0, max(data, d => +d.population)])
+    .range([0,height]);
+
   return (
-    <div>
-      <p>Check console for some nice data</p>
-    </div>
+    <svg width = {width} height = {height}>
+      {data.map(d => <rect 
+        x={xScale(d.income)} 
+        y={height - yScale(+d.population)} 
+        width={xScale.bandwidth()} 
+        height={height}
+        />
+      )}
+    </svg>
   );
-};
+}
 
 export default App;
