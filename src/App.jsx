7ce69c9b3fa8c8031f7602d994ae505068a812a9
Hanna Snarberg <hannasnaris@gmail.com>
2023-02-13 10:29:37 +0100

    Retrieve and console-log data

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import React, { useState, useCallback } from 'react';
+import React, { useEffect } from 'react';
+import { csv } from 'd3';
 
-const width = 960;
-const height = 500;
-const circleRadius = 30;
-const initialMousePosition = { x: width / 2, y: height / 2 };
+const csvUrl =
+  'https://gist.githubusercontent.com/Amandabru/00e96eaa56143e6499d1c651bac03aa8/raw/58ce042b4504d9b660bb93693e47b966cc2eb34f/GapminderData.csv?fbclid=IwAR21ILavgwgRxV4nzOa4tL-KAlwzUElOyUSlaw2lDXhdtYljSdIiXFH5GuM';
 
 const App = () => {
-  const [mousePosition, setMousePosition] = useState(initialMousePosition);
-  const handleMouseMove = useCallback(
-    (event) => {
-      const { clientX, clientY } = event;
-      setMousePosition({ x: clientX, y: clientY });
-    },
-    [setMousePosition]
-  );
-
+  useEffect(() => {
+    csv(csvUrl).then((data) => {
+      console.log(data);
+    });
+  }, []);
   return (
-    <svg width={width} height={height} onMouseMove={handleMouseMove}>
-      <circle cx={mousePosition.x} cy={mousePosition.y} r={circleRadius} />
-    </svg>
+    <div>
+      <p>Check console for some nice data</p>
+    </div>
   );
 };
 
