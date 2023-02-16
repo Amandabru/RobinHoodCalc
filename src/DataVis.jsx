b90fb12d8d26d34c73936a0657aacd579a4fca1a
Hanna Snarberg <hannasnaris@gmail.com>
2023-02-16 14:58:24 +0100

    make code prettier

--- src/DataVis.jsx
+++ src/DataVis.jsx
@@ -7,29 +7,31 @@ import AreaChart from './AreaChart';
 const csvUrl =
   'https://gist.githubusercontent.com/Amandabru/00e96eaa56143e6499d1c651bac03aa8/raw/58ce042b4504d9b660bb93693e47b966cc2eb34f/GapminderData.csv';
 
+const closestIndex = (arr, num) => {
+  let curr = arr[0].income;
+  let diff = Math.abs(num - curr);
+  let index = 0;
+  for (let val = 0; val < arr.length; val++) {
+    let newdiff = Math.abs(num - arr[val].income);
+    if (newdiff < diff) {
+      diff = newdiff;
+      curr = arr[val].income;
+      index = val;
+    }
+  }
+  return index;
+};
+
 const DataVis = () => {
   const [data, setData] = useState(null);
   const [taxRate, setTaxRate] = useState(0);
   const [csvData, setCsvData] = useState(null);
 
-  const closestIndex = (arr, num) => {
-    let curr = arr[0].income;
-    let diff = Math.abs(num - curr);
-    let index = 0;
-    for (let val = 0; val < arr.length; val++) {
-      let newdiff = Math.abs(num - arr[val].income);
-      if (newdiff < diff) {
-        diff = newdiff;
-        curr = arr[val].income;
-        index = val;
-      }
-    }
-    return index;
-  };
-
   const updateData = () => {
     const level4 = 100;
     var collectedTax = 0;
+
+    // Copy original distributition
     var newData = csvData.map((a) => {
       return { ...a };
     });
@@ -48,7 +50,7 @@ const DataVis = () => {
         }
       }
     }
-    // Count number of "brackets" below level 4
+    // Count population below level 4
     var population = 0;
     for (let i = 0; i < csvData.length; i++) {
       if (csvData[i].income <= level4) {
