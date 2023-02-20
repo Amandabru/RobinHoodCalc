4ce526a7ad1fe0d1ed1ce8189d5a8d3f105ba010
Hanna Snarberg <hannasnaris@gmail.com>
2023-02-20 15:59:43 +0100

    Gustavs changes

--- src/DataVis.jsx
+++ src/DataVis.jsx
@@ -11,74 +11,81 @@ const DataVis = () => {
   const [taxRate, setTaxRate] = useState(0);
   const [csvData, setCsvData] = useState(null);
 
-    const collectFromTheRich = (data, incomeMin) => {
-      var collectedTax = 0;
-      for (let i = 0; i < csvData.length; i++) {
-        if (csvData[i].income > incomeMin) {
-          const newIncome =
-            csvData[i].income - (csvData[i].income - incomeMin) * taxRate;
-          collectedTax +=
-            (csvData[i].income - incomeMin) * taxRate * csvData[i].population;
-          var indexNewIncome = closestIndex(csvData, newIncome);
+  const collectFromTheRich = (data, incomeMin) => {
+    var collectedTax = 0;
+    for (let i = 0; i < csvData.length; i++) {
+      if (csvData[i].income > incomeMin) {
+        const newIncome =
+          csvData[i].income - (csvData[i].income - incomeMin) * taxRate;
+        collectedTax +=
+          (csvData[i].income - incomeMin) * taxRate * csvData[i].population;
+        var indexNewIncome = closestIndex(csvData, newIncome);
 
-          if (indexNewIncome !== i) {
-            data[indexNewIncome].population += data[i].population;
-            data[i].population = 0;
-          }
-          else{
-            var collectedFromBracket = (csvData[i].income - incomeMin) * taxRate * csvData[i].population;
-            var incomeDiff = csvData[i].income - csvData[i - 1].income;
-            var movePop = collectedFromBracket / incomeDiff;
-            data[i].population = data[i].population - (movePop);
-            data[i-1].population = data[i-1].population + (movePop);
-          }
+        if (indexNewIncome !== i) {
+          data[indexNewIncome].population += data[i].population;
+          data[i].population = 0;
+        } else {
+          var collectedFromBracket =
+            (csvData[i].income - incomeMin) * taxRate * csvData[i].population;
+          var incomeDiff = csvData[i].income - csvData[i - 1].income;
+          var movePop = collectedFromBracket / incomeDiff;
+          data[i].population = data[i].population - movePop;
+          data[i - 1].population = data[i - 1].population + movePop;
         }
       }
-      return [collectedTax, data];
-    };
+    }
+    return [collectedTax, data];
+  };
 
   const giveToThePoor = (data, incomeMax, collectedTax) => {
-      var population = 0;
-      var spentTax = 0;
-      for (let i = 0; i < csvData.length; i++) {
-        if (csvData[i].income <= incomeMax) {
-          population += csvData[i].population;
-        }
+    var population = 0;
+    var spentTax = 0;
+    for (let i = 0; i < csvData.length; i++) {
+      if (csvData[i].income <= incomeMax) {
+        population += csvData[i].population;
       }
-      // Tar ut pengar per person från insamlade pengar uttdelat på alla under incomeMax
-      var popShare = collectedTax/population;
-      for (let i = 0; i < csvData.length; i++) {
-        var reverseIndex = csvData.length - i;
-        if (i < data.length && i > 0){
+    }
+    // Tar ut pengar per person från insamlade pengar uttdelat på alla under incomeMax
+    var popShare = collectedTax / population;
+    for (let i = 0; i < csvData.length; i++) {
+      var reverseIndex = csvData.length - i;
+      if (i < data.length && i > 0) {
         if (csvData[reverseIndex].income <= incomeMax) {
           // Tar differensen till nästa nivå av inkomst
           // Om popShare räcker för att flytta alla uppåt. Gör så till närmaste(till vart de hamnar) nivå av inkomst
-            const newIncome2 = csvData[reverseIndex].income + collectedTax / population;
-            var indexNewIncome = closestIndex(csvData, newIncome2);
-                    if (indexNewIncome !== reverseIndex) {
-                      spentTax += data[reverseIndex].population * (data[indexNewIncome].income - data[reverseIndex].income);
-                      data[indexNewIncome].population += data[reverseIndex].population;
-                      data[reverseIndex].population = 0;
-                    }
-                    else{
-                      var incomeDiff = csvData[reverseIndex+1].income - csvData[reverseIndex].income;
-                      var movePop = popShare / incomeDiff;
-                      spentTax += movePop * (data[reverseIndex+1].income - data[reverseIndex].income)
-                      data[reverseIndex].population = data[reverseIndex].population - (movePop);
-                      data[reverseIndex + 1].population = data[reverseIndex + 1].population + (movePop);
-                    }
+          const newIncome2 =
+            csvData[reverseIndex].income + collectedTax / population;
+          var indexNewIncome = closestIndex(csvData, newIncome2);
+          if (indexNewIncome !== reverseIndex) {
+            spentTax +=
+              data[reverseIndex].population *
+              (data[indexNewIncome].income - data[reverseIndex].income);
+            data[indexNewIncome].population += data[reverseIndex].population;
+            data[reverseIndex].population = 0;
+          } else {
+            var incomeDiff =
+              csvData[reverseIndex + 1].income - csvData[reverseIndex].income;
+            var movePop = popShare / incomeDiff;
+            spentTax +=
+              movePop *
+              (data[reverseIndex + 1].income - data[reverseIndex].income);
+            data[reverseIndex].population =
+              data[reverseIndex].population - movePop;
+            data[reverseIndex + 1].population =
+              data[reverseIndex + 1].population + movePop;
           }
         }
       }
+    }
     let counter = 0;
     for (let i = 0; i < csvData.length; i++) {
-        counter += data[i].population;
+      counter += data[i].population;
     }
 
-    console.log("Population" + counter);
-    console.log("taxrate" + taxRate)
-    console.log("collectedTax: " + collectedTax)
-    console.log("Spent tax:" + spentTax)
+    console.log('Population' + counter);
+    console.log('taxrate' + taxRate);
+    console.log('collectedTax: ' + collectedTax);
+    console.log('Spent tax:' + spentTax);
     return data;
   };
 
@@ -108,16 +115,15 @@ const DataVis = () => {
     for (let i = 0; i < csvData.length; i++) {
       incomeBefore += csvData[i].income * csvData[i].population;
     }
-    console.log("Tot income before:" + incomeBefore);
+    console.log('Tot income before:' + incomeBefore);
 
     var incomeAfter = 0;
     for (let i = 0; i < csvData.length; i++) {
       incomeAfter += newData[i].income * newData[i].population;
     }
-    console.log("Tot income after:" + incomeAfter);
+    console.log('Tot income after:' + incomeAfter);
     var differeance = incomeAfter - incomeBefore;
-    console.log("World wealth diff: " + differeance);
-
+    console.log('World wealth diff: ' + differeance);
   };
 
   useEffect(() => {
