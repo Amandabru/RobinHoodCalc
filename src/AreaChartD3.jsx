196aa7ca1392c6d4e167d2793f1a004e3808d455
hannaalmqvist <70328827+hannaalmqvist@users.noreply.github.com>
2023-02-26 18:51:06 +0100

    Update AreaChartD3.jsx

--- src/AreaChartD3.jsx
+++ src/AreaChartD3.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 import {
   select,
   scaleLinear,
@@ -11,7 +11,7 @@ import {
   axisRight,
   curveMonotoneX,
   brushX,
-} from 'd3';
+} from "d3";
 
 const AreaChartD3 = ({ data, ExtremePovertyCount }) => {
   const svgRef = useRef();
@@ -25,31 +25,38 @@ const AreaChartD3 = ({ data, ExtremePovertyCount }) => {
 
   useEffect(() => {
     const svg = select(svgRef.current)
-      .attr('width', w)
-      .attr('height', h)
-      .style('overflow', 'visible')
-      .style('margin-top', '30')
-      .style('margin-left', '100')
-      .style('margin-bottom', '50');
+      .attr("width", w)
+      .attr("height", h)
+      .style("overflow", "visible")
+      .style("margin-top", "30")
+      .style("margin-left", "100")
+      .style("margin-bottom", "50");
 
-    svg.selectAll('*').remove();
+    svg.selectAll("*").remove();
 
     const AxisXformat = [
-      '1',
-      '10',
-      '100',
-      '1000',
-      '10000',
-      '100000',
-      '1000000',
-      '10000000',
-      '100000000',
+      "1",
+      "10",
+      "100",
+      "1000",
+      "10000",
+      "100000",
+      "1000000",
+      "10000000",
+      "100000000",
     ];
 
-    let xScale = scaleLog().domain([minIncome, maxIncome]).range([0, w]).nice();
+    let xScale = scaleLog()
+      .domain([minIncome, maxIncome])
+      .range([0, w])
+      .nice();
 
-    const yScaleLeft = scaleLinear().domain([minPop, maxPop]).range([h, 0]);
-    const yScaleRight = scaleLinear().domain([0, 100]).range([h, 0]);
+    const yScaleLeft = scaleLinear()
+      .domain([minPop, maxPop])
+      .range([h, 0]);
+    const yScaleRight = scaleLinear()
+      .domain([0, 100])
+      .range([h, 0]);
 
     // area chart
     const generateScaledArea = area()
@@ -59,86 +66,92 @@ const AreaChartD3 = ({ data, ExtremePovertyCount }) => {
       .curve(curveMonotoneX);
 
     svg
-      .selectAll('.area')
+      .selectAll(".area")
       .data([data])
-      .join('path')
-      .attr('d', (d) => generateScaledArea(d))
-      .attr('fill', '#ffca34')
-      .attr('stroke', '#ffca34');
+      .join("path")
+      .attr("d", (d) => generateScaledArea(d))
+      .attr("fill", "#ffca34")
+      .attr("stroke", "#ffca34");
 
     // extreme poverty line
     svg
-      .append('line')
-      .attr('stroke', 'grey')
-      .attr('x1', xScale(2))
-      .attr('y1', 0)
-      .attr('x2', xScale(2))
-      .attr('y2', h)
-      .style('stroke-dasharray', '3, 3');
+      .append("line")
+      .attr("stroke", "grey")
+      .attr("x1", xScale(2))
+      .attr("y1", 0)
+      .attr("x2", xScale(2))
+      .attr("y2", h)
+      .style("stroke-dasharray", "3, 3");
 
     // x axis label
     svg
-      .style('font', '12px times')
-      .append('text')
-      .attr('class', 'x label')
-      .attr('text-anchor', 'end')
-      .attr('x', w)
-      .attr('y', h + 35)
-      .text('Income ($/day)');
+      .style("font", "12px times")
+      .append("text")
+      .attr("class", "x label")
+      .attr("text-anchor", "end")
+      .attr("x", w)
+      .attr("y", h + 35)
+      .text("Income ($/day)");
 
     // left y axis label
     var axisLabelXLeft = -35;
     var axisLabelY = -25;
     svg
-      .append('text')
+      .append("text")
       .attr(
-        'transform',
-        'translate(' + axisLabelXLeft + ', ' + axisLabelY + ')'
+        "transform",
+        "translate(" + axisLabelXLeft + ", " + axisLabelY + ")"
       )
-      .attr('class', 'y label')
-      .attr('text-anchor', 'middle')
-      .attr('y', 6)
-      .attr('dy', '.75em')
-      .text('Population (%)');
-
-    var axisLabelXRight = w + 30;
+      .attr("class", "y label")
+      .attr("text-anchor", "middle")
+      .attr("y", 6)
+      .attr("dy", ".75em")
+      .text("Population (%)");
 
     svg
-      .append('text')
-      .attr(
-        'transform',
-        'translate(' + axisLabelXRight + ', ' + axisLabelY + ')'
-      )
-      .attr('class', 'y label')
-      .attr('text-anchor', 'middle')
-      .attr('y', 6)
-      .attr('dy', '.75em')
-      .text('Equality tax (%)');
+      .append("text")
+      .attr("transform", "translate(" + w + ", " + axisLabelY + ")")
+      .attr("class", "y label")
+      .attr("text-anchor", "middle")
+      .attr("y", 6)
+      .attr("dy", ".75em")
+      .text("Robinhood tax (%)");
 
     svg
-      .append('text')
-      .attr('x', -270)
-      .attr('y', xScale(1.5))
-      .attr('font-size', 14)
-      .attr('fill', 'gray')
-      .text('People in extreme poverty: ' + ExtremePovertyCount)
-      .attr('transform', 'rotate(-90)');
+      .append("text")
+      .attr("x", -270)
+      .attr("y", xScale(1.5))
+      .attr("font-size", 14)
+      .attr("fill", "gray")
+      .text("People in extreme poverty: " + ExtremePovertyCount)
+      .attr("transform", "rotate(-90)");
 
     // axis
-    const xAxis = axisBottom().scale(xScale).tickValues(AxisXformat);
+    const xAxis = axisBottom()
+      .scale(xScale)
+      .tickValues(AxisXformat);
 
     const yAxisLeft = axisLeft().scale(yScaleLeft);
 
     const yAxisRight = axisRight().scale(yScaleRight);
 
     // data circles
-    svg.append('g').call(xAxis).attr('transform', `translate(0, ${h})`);
+    svg
+      .append("g")
+      .call(xAxis)
+      .attr("transform", `translate(0, ${h})`);
 
-    svg.append('g').call(yAxisLeft);
+    svg.append("g").call(yAxisLeft);
 
-    svg.append('g').call(yAxisRight).attr('transform', `translate(${w}, 0)`);
+    svg
+      .append("g")
+      .call(yAxisRight)
+      .attr("transform", `translate(${w}, 0)`);
 
-    svg.append('g').call(yAxisRight).attr('transform', `translate(${w}, 0)`);
+    svg
+      .append("g")
+      .call(yAxisRight)
+      .attr("transform", `translate(${w}, 0)`);
 
     /*svg
       .selectAll("circle")
@@ -154,13 +167,16 @@ const AreaChartD3 = ({ data, ExtremePovertyCount }) => {
       [w, h],
     ]);
 
-    svg.select('.brush').call(brush).call(brush.move, [1, 100]);
+    svg
+      .select(".brush")
+      .call(brush)
+      .call(brush.move, [1, 100]);
   }, [data]);
 
   return (
     <React.Fragment>
       <svg ref={svgRef}>
-        <g className='brush' />
+        <g className="brush" />
       </svg>
     </React.Fragment>
   );
