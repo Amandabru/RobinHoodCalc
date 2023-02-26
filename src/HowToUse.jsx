2779985997374c4a73bc3ccbf07b81960503a395
Hanna Snarberg <hannasnaris@gmail.com>
2023-02-26 18:59:47 +0100

    New how-to description

--- src/HowToUse.jsx
+++ src/HowToUse.jsx
@@ -5,12 +5,13 @@ const HowToUse = () => {
   return (
     <div className='howToUseContainer'>
       <h1 className> How to Use</h1>
-      <h2>Contoll the tax rate</h2>
+      <h2>The Robin Hood Taxation System</h2>
       <p>
-        Use the sliders to control the marginal tax rate of five different
-        income brackets. All taxed income goes directly to the people at the
-        lowest income levels. See what it takes to move them beyond the line of
-        extreme poverty.
+        The Robin Hood Taxation System allows you as a user to tax the world’s
+        richest and watch as the money moves directly to the people at the
+        lowest income levels. It is a progressive taxation system where you as a
+        user control the <b>marginal rate of taxation</b> for five different
+        income brackets.
       </p>
       <h2>What is marginal tax? </h2>
       <p>
