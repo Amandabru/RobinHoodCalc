24b3f685474d58decfede1101e7942f35d11e1e0
hannaalmqvist <70328827+hannaalmqvist@users.noreply.github.com>
2023-03-03 22:15:32 +0100

    Add divs
    Taxbillionare: add button that adds divs with dropdown (of billionaires) and a remove button

--- src/Pages/Home/ControllTax/TaxBillionaires.jsx
+++ src/Pages/Home/ControllTax/TaxBillionaires.jsx
@@ -1,47 +1,92 @@
-import React from 'react';
-import Toggle from './toggle';
+import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-const TaxBillionaires = ({ billionaires }) => {
-  const logState = (state) => {
-    console.log('Toggled:', state);
-  };
+function TaxBillionaires({ billionaires }) {
+  const [divs, setDivs] = useState([]);
+
+  const options = billionaires.map((item) => item.billionaire);
+
+  function addNewDiv() {
+    // Create a new div element
+    const newDiv = {
+      id: uuidv4(),
+      option: "",
+      selected: false,
+    };
+
+    // Add the new div to the array of divs
+    if (divs.length > 0) {
+      setDivs([newDiv, ...divs]);
+    } else {
+      setDivs([...divs, newDiv]);
+    }
+  }
+
+  function handleOptionChange(e, id) {
+    const value = e.target.value;
+    if (value !== "") {
+      // If a dropdown is selected, add a new div with a default value of ""
+      const newDiv = {
+        id: uuidv4(),
+        option: "",
+        selected: false,
+      };
+      setDivs([...divs, newDiv]);
+    }
+    const newDivs = divs.map((div) => {
+      if (div.id === id) {
+        return { ...div, option: value, selected: true };
+      } else {
+        return div;
+      }
+    });
+    setDivs(newDivs);
+  }
+
+  function removeDiv(id) {
+    const newDivs = divs.filter((div) => div.id !== id);
+    setDivs(newDivs);
+  }
 
   return (
-    <div
-      style={{
-        border: '1px solid black',
-        width: '80%',
-        margin: '50px',
-      }}
-    >
-      <button style={{ cursor: 'pointer', float: 'right', margin: '5px' }}>
-        Clear All
+    <div>
+      <button onClick={addNewDiv} disabled={divs.length >= options.length}>
+        Add new div
       </button>
-      <header style={{ position: 'relative', textAlign: 'center' }}>
-        <h2 style={{ textDecoration: 'underline' }}>
-          Individual Tax for Billionaires
-          <div
-            className='info'
-            style={{
-              marginLeft: '10px',
-              color: 'gray',
-            }}
-          >
-            ?
-            <span className='infoText'>
-              Add specific billionaires to assign them individual taxes
-            </span>
-          </div>
-          <button> + Add</button>
-        </h2>
-      </header>
-      <div>
-        <div>
-          <Toggle toggled={true} onClick={logState} />
+      {divs.map((div) => (
+        <div key={div.id}>
+          {div.selected ? (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <p>{div.option}</p>
+              <button onClick={() => removeDiv(div.id)}>Remove div</button>
+            </div>
+          ) : (
+            <select
+              value={div.option}
+              onChange={(e) => handleOptionChange(e, div.id)}
+            >
+              <option value="">Select an option</option>
+              {options.map((option, index) => (
+                <option
+                  key={index}
+                  value={option}
+                  disabled={divs.some(
+                    (d) => d.option === option && d.id !== div.id
+                  )}
+                >
+                  {option}
+                </option>
+              ))}
+            </select>
+          )}
         </div>
-      </div>
+      ))}
     </div>
   );
-};
+}
 
 export default TaxBillionaires;
+
+/* Write this in home.jsx to make the element visible <TaxBillionaires
+        billionaires={billionaires ? billionaires : billionairesUrl}
+      /> */
