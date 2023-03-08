05efb17010695e2f5a2d2c57f7fd0fe7f3a77590
Frida <97102212+frjda@users.noreply.github.com>
2023-03-08 10:37:06 +0100

    Update levelCounter.js

--- src/Pages/Home/Utils/levelCounter.js
+++ src/Pages/Home/Utils/levelCounter.js
@@ -11,7 +11,7 @@ const levelCounter = (x1Value, x2Value, [...data]) => {
     }
   }
 
-  people < 100 ? (people = "few") : (people = addAbbrevations(people));
+  people < 1000 ? (people = "few") : (people = addAbbrevations(people));
 
   return people;
 };
