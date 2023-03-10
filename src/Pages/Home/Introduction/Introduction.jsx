989845c2b247cd241bd10dc6f6d3dc453b71aa45
Amanda Brundin <abrundin@kth.se>
2023-03-10 16:10:13 +0100

    about

--- src/Pages/Home/Introduction/Introduction.jsx
+++ src/Pages/Home/Introduction/Introduction.jsx
@@ -9,7 +9,7 @@ import {
 const Introduction = ({ data, defaultData, totalCollectedMoney }) => {
   return (
     <div className='introduction'>
-      Welcome to the <b>Robin Hood Calculator!</b> Here you can investigate how
+      Welcome to the <b>Robin Hood Calculator!</b> Here you can explore how
       the wealth distribution of the world would look like when taking from the
       rich and giving to the poor. Use the sliders to the right and se what
       happens!
