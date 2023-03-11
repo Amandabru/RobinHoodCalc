c54412320ec1976a52a8dc13ccf0e815000e4107
hannaalmqvist <70328827+hannaalmqvist@users.noreply.github.com>
2023-03-11 18:17:19 +0100

    Introduction text modified

--- src/Pages/Home/Introduction/Introduction.jsx
+++ src/Pages/Home/Introduction/Introduction.jsx
@@ -1,30 +1,30 @@
-import React from 'react';
-import './introduction.css';
+import React from "react";
+import "./introduction.css";
 import {
   extremePovertyCounter,
   formatNumbers,
   extremePovertyPercentage,
-} from '../Utils/index';
+} from "../Utils/index";
 
 const Introduction = ({ data, defaultData, totalCollectedMoney }) => {
   return (
-    <div className='introduction'>
-      Welcome to the <b>Robin Hood Calculator!</b> Here you can explore how
-      the daily income distribution in the world would look like when take from the
-      rich and give to the poor. Interact with the Robin Hood Taxation System 
-      to the right and see what happens!
+    <div className="introduction">
+      Welcome to the <b>Robin Hood Calculator!</b> Here you can explore how the
+      daily income distribution in the world would look like when take from the
+      rich and give to the poor. Interact with the Robin Hood Taxation System to
+      the right and the shart to see what happens!
       <br></br>
       <br></br>
-        <div className='displayInfo'>
-        You have now brought{' '}
+      <div className="displayInfo">
+        You have now brought{" "}
         <b>
-          {extremePovertyPercentage(data) === '0%'
-            ? 'ALL'
+          {extremePovertyPercentage(data) === "0%"
+            ? "ALL"
             : (
                 extremePovertyCounter(defaultData) - extremePovertyCounter(data)
-              ).toLocaleString('en-US')}
-        </b>{' '}
-        people out of extreme poverty by redistributing{' '}
+              ).toLocaleString("en-US")}
+        </b>{" "}
+        people out of extreme poverty by redistributing{" "}
         <b>{formatNumbers(totalCollectedMoney)}</b>$.
       </div>
     </div>
