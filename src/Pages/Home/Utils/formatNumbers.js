03749c158d1cf673fc5cfdce96a55c83859631cd
Frida <97102212+frjda@users.noreply.github.com>
2023-03-11 16:22:42 +0100

    Update formatNumbers.js

--- src/Pages/Home/Utils/formatNumbers.js
+++ src/Pages/Home/Utils/formatNumbers.js
@@ -2,8 +2,7 @@ function formatNumbers(n) {
   if (n < 1e3) return n;
   if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(0) + "k";
   if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-  if (n >= 1e9 && n < 7990000000) return +(n / 1e9).toFixed(2) + "B";
-  if (n >= 7990000000 && n < 1e12) return "> " + (n / 1e9).toFixed(0) + "B";
+  if (n >= 1e9) return +(n / 1e9).toFixed(2) + "B";
 }
 
 export default formatNumbers;
