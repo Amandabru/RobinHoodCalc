4dc128813cd520111f215f7558a399ec6a21944c
Hanna Snarberg <hannasnaris@gmail.com>
2023-03-13 14:24:15 +0100

    Info text about distribution

--- src/Pages/Home/Introduction/RadioButton.jsx
+++ src/Pages/Home/Introduction/RadioButton.jsx
@@ -12,15 +12,25 @@ const RadioButton = ({selectedOption, onChange}) => {
 
   return (
     <div className="radioButtonsContainer">
-    <label className="radioButtonContainer">Smooth Distribution
-        <input type="checkbox" value={'Avoid Population Accumulation'} onChange={(e) => handleOptionChange(e)} checked={option === 'Avoid Population Accumulation'} />
-        <span className="checkmark"></span>
-    </label>
-    <label className="radioButtonContainer">Strict Distribution
-        <input type="checkbox" value={'To Lowest Paid Only'} onChange={(e) => handleOptionChange(e)} checked={option === 'To Lowest Paid Only'}/>
-        <span className="checkmark"></span>
-    </label>
-  </div>
+        <div className="distributionInfo" style={{marginLeft: "5px", color: "gray"}}>
+          <label className="radioButtonContainer"> Smooth Distribution
+            <input type="checkbox" value={'Avoid Population Accumulation'} onChange={(e) => handleOptionChange(e)} checked={option === 'Avoid Population Accumulation'}/>
+            <span className="checkmark"></span>
+          </label>
+          <span className="distributionText">
+            Distributes to the poorest first, but without accumulating people at the lowest income.
+          </span>
+        </div>
+        <div className="distributionInfo" style={{marginLeft: "5px", color: "gray"}}>
+          <label className="radioButtonContainer">Strict Distribution
+            <input type="checkbox" value={'To Lowest Paid Only'} onChange={(e) => handleOptionChange(e)} checked={option === 'To Lowest Paid Only'}/>
+            <span className="checkmark"></span>
+          </label>
+          <span className="distributionText">
+            Distributes strictly to the very poorest, cumulatively moving them up the income axis.
+          </span>
+        </div>
+    </div>
   )
 }
 
