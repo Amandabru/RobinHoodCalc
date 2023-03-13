57cf352846ca6c62dcccaff5c4c3fc1f8d25e3de
Frida <97102212+frjda@users.noreply.github.com>
2023-03-13 08:34:56 +0100

    format numbers <1000

--- src/Pages/Home/Utils/formatNumbers.js
+++ src/Pages/Home/Utils/formatNumbers.js
@@ -1,5 +1,5 @@
 function formatNumbers(n) {
-  if (n < 1e3) return n;
+  if (n < 1e3) return n.toFixed(0);
   if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(0) + "k";
   if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
   if (n >= 1e9) return +(n / 1e9).toFixed(2) + "B";
